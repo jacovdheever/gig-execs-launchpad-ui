6eb56351c967130d58ac7ab1fdd29cae9b0c330d
Jaco van den Heever <jaco@sanddollardesign.co.za>
2025-08-13 18:39:01 +0200

    security: remove scripts and sql folders from git tracking to prevent secrets exposure

--- sql/temp_disable_reference_fk.sql
+++
@@ -1,8 +0,0 @@
--- Temporarily disable foreign key constraint for reference_contacts table
--- This allows us to insert data and then recreate the constraint
-
--- Drop the foreign key constraint temporarily
-ALTER TABLE reference_contacts
-DROP CONSTRAINT IF EXISTS reference_contacts_user_id_fkey;
-
--- Note: After migration is complete, run the fix script to recreate the constraint 
