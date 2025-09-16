18c4ccfdaf6945ce3597419ccd7930e84cbbe44e
Jaco van den Heever <jaco@sanddollardesign.co.za>
2025-09-16 17:12:27 +0200

    🔧 ADD GLOBAL USER TYPE: Fix ProfileEdit runtime reference
    - Added global User interface declaration to resolve runtime error
    - ProfileEdit component expects global User type to be available
    - This was likely working before due to different build/environment
    - Should resolve 'ReferenceError: User is not defined' at line 745

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Global User type to resolve ProfileEdit runtime references
+declare global {
+  interface User {
+    id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    user_type: string;
+    vetting_status?: string;
+  }
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
